d27e60643bc9c77cc05b0da0e5c314c2d18189a8
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 21:28:41 +0300

    BackupConfigurationModel.kt added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/backup/configurations/BackupConfigurationModel.kt
@@ -0,0 +1,14 @@
+package com.mairwunnx.projectessentials.backup.configurations
+
+data class BackupConfigurationModel(
+    var backupEnabled: Boolean = true,
+    var firstLaunchDelay: Boolean = true,
+    var backupConfigurations: Boolean = true,
+    var backupCreationDelay: Int = 300,
+    var backupCompressionLevel: Int = 3,
+    var backupDirectoryPath: String = "backup",
+    var backupDateFormat: String = "yyyy-MM-dd_HH.mm.ss",
+    var maxBackupFiles: Int = 10,
+    var rollingBackupFilesEnabled: Boolean = true,
+    var removeExtraFiles: Boolean = true
+)
