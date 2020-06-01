bbc2c21a4e3b5af8067054a369590368d8e90b16
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 21:30:48 +0300

    BackupConfiguration.kt implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/backup/configurations/BackupConfigurationModel.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/backup/configurations/BackupConfigurationModel.kt
@@ -1,5 +1,8 @@
 package com.mairwunnx.projectessentials.backup.configurations
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BackupConfigurationModel(
     var backupEnabled: Boolean = true,
     var firstLaunchDelay: Boolean = true,
